fecb473f465de22dc8b8a8ce4a3a134b2fe428d1
Darshan Prajapati <darshanprajapati@Darshans-MacBook-Air.local>
2023-03-15 10:40:09 +0530

    changes in header

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -1,16 +1,16 @@
-<?php 
-  
-namespace App\Http\Controllers\Auth; 
-  
+<?php
+
+namespace App\Http\Controllers\Auth;
+
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request; 
-use DB; 
-use Carbon\Carbon; 
-use App\User; 
-use Mail; 
+use Illuminate\Http\Request;
+use DB;
+use Carbon\Carbon;
+use App\User;
+use Mail;
 use Hash;
 use Illuminate\Support\Str;
-  
+
 class ForgotPasswordController extends Controller
 {
       /**
@@ -22,7 +22,7 @@ class ForgotPasswordController extends Controller
       {
          return view('auth.forgetPassword');
       }
-  
+
       /**
        * Write code on Method
        *
@@ -33,20 +33,20 @@ class ForgotPasswordController extends Controller
           $request->validate([
               'email' => 'required|email|exists:users',
           ]);
-  
+
           $token = Str::random(64);
-  
+
           DB::table('password_resets')->insert([
-              'email' => $request->email, 
-              'token' => $token, 
+              'email' => $request->email,
+              'token' => $token,
               'created_at' => Carbon::now()
             ]);
-  
+
           Mail::send('email.forgetPassword', ['token' => $token], function($message) use($request){
               $message->to($request->email);
               $message->subject('Reset Password');
           });
-  
+
           return back()->with('message', 'We have e-mailed your password reset link!');
       }
       /**
@@ -54,10 +54,10 @@ class ForgotPasswordController extends Controller
        *
        * @return response()
        */
-      public function showResetPasswordForm($token) { 
+      public function showResetPasswordForm($token) {
          return view('auth.forgetPasswordLink', ['token' => $token]);
       }
-  
+
       /**
        * Write code on Method
        *
@@ -70,23 +70,23 @@ class ForgotPasswordController extends Controller
               'password' => 'required|string|min:6|confirmed',
               'password_confirmation' => 'required'
           ]);
-  
+
           $updatePassword = DB::table('password_resets')
                               ->where([
-                                'email' => $request->email, 
+                                'email' => $request->email,
                                 'token' => $request->token
                               ])
                               ->first();
-  
+
           if(!$updatePassword){
               return back()->withInput()->with('error', 'Invalid token!');
           }
-  
+
           $user = User::where('email', $request->email)
                       ->update(['password' => Hash::make($request->password)]);
- 
+
           DB::table('password_resets')->where(['email'=> $request->email])->delete();
-  
-          return redirect('/login')->with('message', 'Your password has been changed!');
+
+          return redirect('/user/login')->with('message', 'Your password has been changed!');
       }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,9 +23,13 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <div class="main-logo"><a class="navbar-brand" href="{{ url('/') }}"><img
+                    src="{{ asset('/frontend/img/dhiiraaya-logo.svg') }}" style="width:150px" alt="">
+            </a>
+        </div>
+                {{-- <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
-                </a>
+                </a> --}}
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
