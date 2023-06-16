015b076089eee67b77fcea87744911bc8f10756c
Darshan Prajapati <darshanprajapati@Darshans-MacBook-Air.local>
2023-06-16 11:58:15 +0530

    social login

--- config/services.php
+++ config/services.php
@@ -35,14 +35,20 @@ return [
         'redirect' => 'http://localhost:8000/login/github/callback',
      ],
      'google' => [
-        'client_id' => '749894282707-m6vpb2at63ag3jinatr6ldkcgdkc558n.apps.googleusercontent.com', //Google API
-        'client_secret' => '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', //Google Secret
-        'redirect' => 'http://localhost:8000/login/google/callback',
+        'client_id' => '231165282567-gnniqdoma77oqgao1nd6i7scke5c7d8o.apps.googleusercontent.com', //Google API
+        'client_secret' => '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', //Google Secret
+        'redirect' => 'https://dhiiraayarealty.com/login/google/callback',
      ],
+
+    //  'facebook' => [
+    //     'client_id' => '1494792524037846', //Facebook API
+    //     'client_secret' => '1bf13c2faf680f4ae2b97bdafa92c7d2', //Facebook Secret
+    //     'redirect' => 'http://localhost:8000/login/facebook/callback',
+    //  ],
      'facebook' => [
-        'client_id' => '1250400699161242', //Facebook API
-        'client_secret' => '1d2a36ccf85b755606d1bc1fe003e4b2', //Facebook Secret
-        'redirect' => 'http://localhost:8000/login/facebook/callback',
+        'client_id' => '146965691719072', //Facebook API
+        'client_secret' => '893186aa67704d6f83533b6124db6757', //Facebook Secret
+        'redirect' => 'https://dhiiraayarealty.com/login/facebook/callback',
      ],
 
 
